17ccc87da38ad74f859645524712a1fb23368baa
tomasklima <tomas.klim@gmail.com>
2020-06-27 10:34:57 +0200

    [upd] Build simple tree from simple form

---
+++ src/model/ENode.ts
@@ -0,0 +1,21 @@
+export interface ENodeData {
+  '@id': string;
+  '@type': string;
+  has_related_question: Array<string>;
+  'has-layout-class': Array<string> | string;
+  'has-preceding-question': string;
+  'is-relevant-if': string;
+  'rdfs:label': string;
+}
+
+class ENode {
+  public parent: string | null;
+  public data: ENodeData;
+
+  constructor(parent: string | null, data: any) {
+    this.parent = parent;
+    this.data = data;
+  }
+}
+
+export default ENode;
